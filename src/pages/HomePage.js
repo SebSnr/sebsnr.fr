ae103d5c12cb18c335b16a69982d4a6ff6b91f79
Seb <sanner.sebastien@gmail.com>
2021-09-08 00:14:47 +0200

    :sparkles: add carousel

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import HomeBanner from '../components/HomeBanner'
 import Navigation from '../components/Navigation'
+import SkillsCarousel from '../components/SkillsCarousel'
 // components
 
 export default function HomePage() {
     return (
         <React.Fragment>
             <Navigation />
-            <HomeBanner />        
+            <HomeBanner />
+            <SkillsCarousel />        
         </React.Fragment>
     )
 }
