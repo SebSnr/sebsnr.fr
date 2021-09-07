4993d542c1d2876ab8ee90e271bf7e7a1f6973de
Seb <sanner.sebastien@gmail.com>
2021-09-07 19:06:40 +0200

    :art: improve banner + nav

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -5,19 +5,22 @@ export default function HomeBanner() {
 	return (
 		<div className="home-banner container d-flex flex-wrap">
 			<div className="home-banner__left">
-                {/* <div> */}
-				    <img src="./images/profile.png" alt="profile" className="z-index-1" />
-                {/* </div> */}
+				<img src="./images/profile.png" alt="profile" className="z-index-1" />
 			</div>
 			<div className="home-banner__right">
-				<strong>SEBASTIEN SANNER</strong>
-				<span>Développeur web Javascript React / NodeJS</span>
-				<p>
-					“ Féru d’informatique, et également de sport, de voyage et bien d’autres, je suis de nature entreprenant et bienveillant.
-					<br />
-					Véritable couteau suisse de par mes multiples expériences, je suis un éternel apprenant. La programmation m’apporte ainsi ma dose de challenges et de réflexion. “
-				</p>
-				<HashLink to="#mesprojets">Mes projets</HashLink>
+				
+					<div>
+						<span className="prenom ">Sebastien</span><br/>
+						<span className="nom">Sanner</span>
+					</div>
+					<h1>Développeur web Javascript React / NodeJS</h1>
+					<blockquote>
+						“  Féru d’informatique, et également de sport, de voyage et bien d’autres, je suis de nature entreprenant et bienveillant.
+						<br />
+						Véritable couteau suisse de par mes multiples expériences, je suis un éternel apprenant. La programmation m’apporte ainsi ma dose de challenges et de réflexion.  “
+					</blockquote>
+					<HashLink to="#mesprojets" className="btn btn--1">Mes projets</HashLink>
+				
 			</div>
 		</div>
 	)
